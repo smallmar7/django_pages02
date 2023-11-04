5d26ec92499f16b36c3b0701ed7d93cf924d3b33
Mario Pascua <mcpascua90@gmail.com>
2023-11-04 10:28:40 -0500

    initial commit

---
+++ django_pages/urls.py
@@ -0,0 +1,10 @@
+# django_pages/urls.py
+from django.contrib import admin
+from django.urls import path, include # new
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('pages.urls')), # new
+]
+
+
